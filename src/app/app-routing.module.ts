f26ecc0f8b1aafac0b7417c986fb23c44afaaf6a
Isaias Colon <isaiascolon00@gmail.com>
2021-04-22 09:29:43 -0500

    first component and routing created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreateComponent } from './pages/doodle/create/create.component';
+import { DoodleComponent } from './pages/doodle/doodle.component';
+import { OptionsComponent } from './pages/doodle/options/options.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', component: AppComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { 
+        path: 'create', component: DoodleComponent,
+        children: [
+          { path: '', component: CreateComponent },
+          { path: 'options', component: OptionsComponent },
+        ]
+       },
+
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { DoodleComponent } from './pages/doodle/doodle.component';
+import { CreateComponent } from './pages/doodle/create/create.component';
+import { OptionsComponent } from './pages/doodle/options/options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DoodleComponent,
+    CreateComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
